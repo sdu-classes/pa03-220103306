145d5cbb9e106fadd956cebcc121ebe6244b9396
220103306 <125125442+220103306@users.noreply.github.com>
2023-02-18 20:31:55 +0600

    Create Main.java

---
+++ problem-A/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main{
+public static void main(String[] args){
+Scanner scan = new Scanner(System.in);
+try{
+            String num1 = scan.next();
+            String num2 = scan.next();
+            int a = Integer.parseInt(num1);
+            int b = Integer.parseInt(num2);
+            Calculator myCalc = new Calculator(a,b);
+            System.out.println(myCalc.Add());
+            System.out.println(myCalc.Subtract());
+            System.out.println(myCalc.Multiplication());
+            System.out.println(myCalc.Division());
+        }catch (NumberFormatException e){
+            System.out.println("Number is not int");
+        }
+        }
+ }
